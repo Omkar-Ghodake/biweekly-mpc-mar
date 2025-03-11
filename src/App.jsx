f2583d5f8d1560103a5b34140f3aca60da0baea0
Omkar-Ghodake <omkar.a.g.78@gmail.com>
2025-03-11 13:07:44 +0530

    added routing and created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router'
 import './App.css'
 
 function App() {
-  return <div className=''>BLACK PAGE</div>
+  return (
+    <Link to={'/landing'} className='overflow-hidden w-[100vh] h-[100vh]'>
+      <div className='h-screen w-screen bg-black'>BLACK PAGE</div>
+    </Link>
+  )
 }
 
 export default App
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,7 +1,70 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='absolute right-0 bottom-0'>
+      <ul className='navlist list-disc'>
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/' className='navlink'>
+            Black
+          </Link>
+        </li>
+        <br />
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/landing' className='navlink'>
+            Landing
+          </Link>
+        </li>
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/team' className='navlink'>
+            Team
+          </Link>
+        </li>
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/tournaments' className='navlink'>
+            Tournaments
+          </Link>
+        </li>
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/scores' className='navlink'>
+            Scores
+          </Link>
+        </li>
+
+        <br />
+
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/login' className='navlink'>
+            Login
+          </Link>
+        </li>
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/admin/dashboard' className='navlink'>
+            Dashboard
+          </Link>
+        </li>
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/admin/dashboard/editTeam' className='navlink'>
+            EditTeam
+          </Link>
+        </li>
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/admin/dashboard/editTournaments' className='navlink'>
+            EditTournaments
+          </Link>
+        </li>
+
+        <br />
+
+        <li className='navitem hover:underline text-blue-500'>
+          <Link to='/exit' className='navlink'>
+            Exit
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Navbar
