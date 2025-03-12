075a41ea5429a3877217ef8f0c34018440e849f8
Shubham Joshi <shubhamdjoshi31@gmail.com>
2025-03-12 17:32:17 +0530

    minor changes

--- src/pages/admin/EditTeam.jsx
+++ src/pages/admin/EditTeam.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import Pointlist from "../../components/Pointlist";
 
 const EditTeam = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
   return (
     <div className="h-screen flex items-center justify-center">
--- src/pages/admin/EditTournaments.jsx
+++ src/pages/admin/EditTournaments.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const EditActivites = () => {
-  return <div>EditActivites</div>
-}
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center">
+        <div className="flex flex-col space-y-3 w-3/4 max-h-40 min-h-5/6 bg-sky-400 p-4 rounded-xl">
+            <div className="h-20 w-32 bg-slate-200 rounded-3xl">
+            </div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga autem consectetur, dolor quasi est vitae reprehenderit praesentium repellat. Error aut reprehenderit delectus at? Suscipit corporis eos, sequi ullam tenetur vel.
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default EditActivites
+export default EditActivites;
