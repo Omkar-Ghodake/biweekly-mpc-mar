78bb2031d8d9a64f6e688410b9bde2512d8051f2
Shubham Joshi <shubhamdjoshi31@gmail.com>
2025-03-13 09:31:01 +0530

    minor edits

--- src/components/Pointlist.jsx
+++ src/components/Pointlist.jsx
@@ -7,7 +7,7 @@ const Pointlist = ({ item, index }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{delay: 0.09 * index}}
-      className="w-10/12 mx-auto p-2 bg-slate-200 cursor-pointer transition-all duration-300 ease-in-out hover:bg-slate-300 hover:scale-102 hover:shadow-lg rounded-3xl grid grid-cols-6 text-center items-center shadow-md"
+      className="w-10/12 mx-auto p-2 bg-slate-200 transition-all duration-300 ease-in-out hover:bg-slate-300 hover:scale-102 hover:shadow-lg rounded-3xl grid grid-cols-6 text-center items-center shadow-md"
     >
       <span className="p-1 col-span-1  border-r-1 border-gray-400">{item}</span>
       <span className="p-1 col-span-3  border-r-1 border-gray-400 text-left ml-2">
--- src/pages/admin/EditTeam.jsx
+++ src/pages/admin/EditTeam.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { motion } from "framer-motion";
 import Pointlist from "../../components/Pointlist";
+import Modal from "../../layouts/Modal/Modal";
+import { ModalContext } from "../../context/ModalProvider";
+import ModalHead from "../../layouts/Modal/ModalHead";
+import ModalBody from "../../layouts/Modal/ModalBody";
+import ball from "../../assets/ball.jpg";
 
 const EditTeam = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  const arr = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+  const { openModal } = useContext(ModalContext);
+
+  const updateDetails = () => {};
 
   return (
     <div className="h-screen flex items-center justify-center">
@@ -21,7 +31,9 @@ const EditTeam = () => {
           <span className="p-1 col-span-1 border-r border-gray-400">
             Sr. no.
           </span>
-          <span className="p-1 col-span-3 border-r border-gray-400 text-left ml-2">Name</span>
+          <span className="p-1 col-span-3 border-r border-gray-400 text-left ml-2">
+            Name
+          </span>
           <span className="p-1 col-span-1 border-r border-gray-400">
             Issue Count
           </span>
@@ -29,12 +41,37 @@ const EditTeam = () => {
         </div>
 
         {/* Animated Pointlist Items */}
-        <div className="list flex flex-col space-y-3">
+        <div
+          className="list flex flex-col space-y-3 cursor-pointer"
+          
+        >
           {arr.map((item, index) => (
-            <Pointlist item={item} index={index} />
+            <Pointlist item={item} index={index} onClick={openModal}/>
           ))}
         </div>
       </motion.div>
+      <Modal>
+        <ModalHead>players profile</ModalHead>
+        <ModalBody>
+          <div>
+            <form action="" onSubmit={updateDetails}>
+              <div className="flex flex-row items-center space-x-5">
+                <img src={ball} alt="" className="w-36 h-36 rounded-full" />
+                <label htmlFor="">Name</label>
+                <input type="text" value="Name" className="border rounded-3xl w-1/2 p-2"/></div>
+                <label htmlFor="">Name</label>
+                <input type="text" value="" />
+                <label htmlFor="">Name</label>
+                <input type="text" value="" />
+                <label htmlFor="">Name</label>
+                <input type="text" value="" />
+                <label htmlFor="">Name</label>
+                <input type="text" value="" />
+              
+            </form>
+          </div>
+        </ModalBody>
+      </Modal>
     </div>
   );
 };
