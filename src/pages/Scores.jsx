7a79a413e9d5f83c2ca8c8fb53da9e611a6ec697
Umakant Patil <umakant@gmail.com>
2025-03-18 18:30:48 +0530

    Score page individual image commit

--- src/pages/Scores.jsx
+++ src/pages/Scores.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import img from "../glb/Members_Photos_Final/Umakant.png";
+
 import { motion } from "framer-motion";
 import { ModalContext } from "../context/ModalProvider";
 import Modal from "../layouts/Modal/Modal";
@@ -19,23 +19,94 @@ const Scores = () => {
   const [selectedPlayer, setSelectedPlayer] = useState(null);
 
   const players = [
-    { id: 1, name: "Virat Kohli", image: img },
-    { id: 2, name: "Rohit Sharma", image: img },
-    { id: 3, name: "MS Dhoni", image: img },
-    { id: 4, name: "Jasprit Bumrah", image: img },
-    { id: 5, name: "Ravindra Jadeja", image: img },
-    { id: 6, name: "KL Rahul", image: img },
-    { id: 7, name: "Hardik Pandya", image: img },
-    { id: 8, name: "Rishabh Pant", image: img },
-    { id: 9, name: "Shubman Gill", image: img },
-    { id: 10, name: "Mohammed Shami", image: img },
-    { id: 12, name: "Yuzvendra Chahal", image: img },
-    { id: 13, name: "Shubman Gill", image: img },
-    { id: 14, name: "Mohammed Shami", image: img },
-    { id: 15, name: "Yuzvendra Chahal", image: img },
-    { id: 16, name: "Mohammed Shami", image: img },
-    { id: 17, name: "Yuzvendra Chahal", image: img },
-    { id: 18, name: "Yuzvendra Chahal", image: img },
+    {
+      id: 1,
+      name: "Vishnu Menon",
+      image: "src/glb/Members_Photos_Final/Vishnu.png",
+    },
+    {
+      id: 2,
+      name: "Anagha Shinde",
+      image: "src/glb/Members_Photos_Final/Anagha.png",
+    },
+    {
+      id: 3,
+      name: "Anshree Shukla",
+      image: "src/glb/Members_Photos_Final/Anushree.png",
+    },
+    {
+      id: 4,
+      name: "Avinash Gupta",
+      image: "src/glb/Members_Photos_Final/Avinash.png",
+    },
+    {
+      id: 5,
+      name: "Bhavya Momaya",
+      image: "src/glb/Members_Photos_Final/Bhavya.png",
+    },
+    {
+      id: 6,
+      name: "Devraj Singh",
+      image: "src/glb/Members_Photos_Final/Devraj.png",
+    },
+    {
+      id: 7,
+      name: "Dhiraj Kunder",
+      image: "src/glb/Members_Photos_Final/Dhiraj.png",
+    },
+    {
+      id: 8,
+      name: "Jaypal Koli",
+      image: "src/glb/Members_Photos_Final/Jaipal.png",
+    },
+    { id: 9, name: "Manoj", image: "src/glb/Members_Photos_Final/Manoj.png" },
+    {
+      id: 10,
+      name: "Mridual ",
+      image: "src/glb/Members_Photos_Final/Mridul.png",
+    },
+    {
+      id: 12,
+      name: "Nikita_Sonawane",
+      image: "src/glb/Members_Photos_Final/Nikita_Sonawane.png",
+    },
+    {
+      id: 13,
+      name: "Nikita_Suhane",
+      image: "src/glb/Members_Photos_Final/Nikita_Suhane.png",
+    },
+    { id: 14, name: "Omkar", image: "src/glb/Members_Photos_Final/Omkar.png" },
+    {
+      id: 15,
+      name: "Prithwi",
+      image: "src/glb/Members_Photos_Final/Prithwi.png",
+    },
+    {
+      id: 16,
+      name: "Rishabh",
+      image: "src/glb/Members_Photos_Final/Rishabh.png",
+    },
+    {
+      id: 17,
+      name: "Sakshi",
+      image: "src/glb/Members_Photos_Final/Sakshi.png",
+    },
+    {
+      id: 18,
+      name: "Sanjeev",
+      image: "src/glb/Members_Photos_Final/Sanjeev.png",
+    },
+    {
+      id: 18,
+      name: "Shubham",
+      image: "src/glb/Members_Photos_Final/Shubham.png",
+    },
+    {
+      id: 18,
+      name: "Umakant",
+      image: "src/glb/Members_Photos_Final/Umakant.png",
+    },
+
     // { id:19,name: "Mohammed Shami", image: img },
     // { id:1,name: "Yuzvendra Chahal", image: img },
     // { id:1,name: "Mohammed Shami", image: img },
@@ -51,7 +122,7 @@ const Scores = () => {
   };
 
   return (
-    <div className="bg-black w-full min-h-screen flex flex-col items-center justify-center ">
+    <div className="bg-black w-full max-h-screen flex flex-col items-center justify-center ">
       {/* Header Section */}
       <div className="w-auto max-h-[950px] min-h-2/3 justify-center items-center  flex flex-col bg-gradient-to-t from-blue-600 to-blue-200 opacity-90 p-1 rounded-tl-3xl rounded-br-3xl border-2 ">
         <div className="w-full bg-gradient-to-t from-blue-600 to-blue-200 opacity-90 rounded-tl-3xl rounded-br-3xl border-2 p-4">
@@ -63,7 +134,7 @@ const Scores = () => {
         </div>
 
         <div className="w-full flex-1  overflow-y-scroll [&::-webkit-scrollbar]:hidden">
-          <div className="grid grid-cols-3 md:grid-cols-5 gap-6 my-3 p-2">
+          <div className="grid grid-cols-3 md:grid-cols-5 gap-6 my-3 p-2 ">
             {players.map((player, index) => (
               <motion.div
                 initial="hidden"
@@ -95,15 +166,9 @@ const Scores = () => {
           </ModalHead>
 
           <ModalBody>
-<<<<<<< HEAD
-            <div className="flex flex-row items-center space-x-5 p-5 relative">
-              <div className="flex flex-col w-2/3 h-96 rounded-lg bg-gradient-to-t from-gray-400 to-blue-200 opacity-90 space-y-3 p-0">
-                <div className="flex flex-row items-center w-auto h-auto justify-center rounded-lg bg-gradient-to-b from-blue-300 to-gray-200 opacity-90 space-x-5 text-2xl font-bold uppercase py-3">
-=======
             <div className="flex flex-row flex-wrap items-center p-5 relative w-full">
               <div className="flex flex-col w-3/5 h-96 rounded-lg bg-gray-200 space-y-3 p-0">
                 <div className="flex flex-row items-center w-auto h-auto justify-center rounded-t-lg bg-amber-200 space-x-5 text-2xl  uppercase py-3 font-bold  text-center text-white bg-gradient-to-b from-sky-600 to-sky-800  shadow-md p-3">
->>>>>>> 01ad03203b7186f48970cfcc981c7d3f5ee26d1e
                   MPC Career
                 </div>
 
@@ -132,20 +197,16 @@ const Scores = () => {
                   </div>
                 </div>
 
-<<<<<<< HEAD
-                <div className="flex flex-row items-center w-auto h-full my-auto justify-center rounded-lg bg-gradient-to-b from-blue-300 to-gray-200 opacity-90 space-x-5  text-2xl font-bold uppercase">
-=======
                 <div className="flex flex-row items-center w-auto h-full my-auto justify-center rounded-b-lg bg-amber-200 space-x-5  text-2xl font-bold uppercase text-white bg-gradient-to-b from-sky-600 to-sky-800  ">
->>>>>>> 01ad03203b7186f48970cfcc981c7d3f5ee26d1e
                   1st Ranking
                 </div>
               </div>
 
               <div className=" w-[350px] flex items-center relative">
                 <img
-                  src={img}
+                  src={selectedPlayer.image}
                   alt="Placeholder Image"
-                  className=" w-full  h-[537px] absolute -top-135 -right-120 z-10"
+                  className=" w-full  h-[537px]  brightness-110 absolute -top-135 -right-120 z-10"
                 />
               </div>
             </div>
