3a6d10edae7fc9856a644207ba7b65d0f6046c16
DevrajSGurjar <dev551093@gmail.com>
2025-03-20 12:19:32 +0530

    login page style changes

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -9,9 +9,9 @@ const Login = () => {
         <div >
 
           <div className='flex flex-col'>
-            <label className='text-black text-sm font-medium mb-2'>Email</label>
+            <label className='text-black text-sm font-medium ml-3 mb-2'>Email</label>
             <input
-              className='w-full p-3 border-2 border-gray-300 rounded-xl bg-transparent focus:outline-none focus:ring-2 focus:ring-green-500 transition-all'
+              className='w-full p-2 border border-gray-300 rounded-2xl bg-transparent focus:outline-none focus:ring-1 focus:ring-green-500 transition-all'
               placeholder='Enter your email'
               type="email"
             />
@@ -19,9 +19,9 @@ const Login = () => {
 
 
           <div className='flex flex-col'>
-            <label className='text-black text-sm font-medium mb-2'>Password</label>
+            <label className='text-black text-sm font-medium ml-3 mt-2 mb-2'>Password</label>
             <input
-              className='w-full p-3 border-2 border-gray-300 rounded-xl bg-transparent focus:outline-none focus:ring-2 focus:ring-green-500 transition-all'
+              className='w-full p-2 border border-gray-300 rounded-2xl bg-transparent focus:outline-none focus:ring-1 focus:ring-green-500 transition-all'
               placeholder='Enter your password'
               type="password"
             />
@@ -29,14 +29,14 @@ const Login = () => {
 
 
           <div className='flex justify-end'>
-            <button className='text-black text-sm hover:underline focus:outline-none'>
+            <button className='text-black mt-1 text-sm hover:underline focus:outline-none'>
               Forgot password?
             </button>
           </div>
 
 
           <div>
-            <button className='w-full py-3 bg-gradient-to-r from-green-400 to-green-500 text-white rounded-xl shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-green-600 transform transition-all hover:scale-105'>
+            <button className='w-full py-2 mt-3 bg-gradient-to-r from-green-400 to-green-500 text-white font-bold rounded-3xl shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-green-600 transform transition-all hover:scale-105'>
               Sign In
             </button>
           </div>
