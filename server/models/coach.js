45ad9df58bbdec6806247e2f8ce5854e610ac58d
Shubham Joshi <shubhamdjoshi31@gmail.com>
2025-03-24 17:56:35 +0530

    added coach model

---
+++ server/models/coach.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const {Schema,model} = mongoose;
+
+const CoachSchema = new Schema({
+    name : {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    domain_name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+      },
+      emp_id: {
+        type: mongoose.Types.Long,
+        required: true,
+        unique: true,
+      },
+      image: {
+        type: String,
+        default: '',
+      },
+      description : {
+        type: String,
+        trim: true,
+      },
+      gender: {
+        type: String,
+        enum: ["Male", "Female"],
+        required: true,
+    },
+})
