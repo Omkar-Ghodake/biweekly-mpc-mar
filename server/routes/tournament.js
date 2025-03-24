21c6546bf95b243013adf1e63dbc1ab0b6151b9d
Vishnu Menon <menonvishnu26@gmail.com>
2025-03-24 18:36:36 +0530

    Fixed URL Parameter in Tournament Router

--- server/routes/tournament.js
+++ server/routes/tournament.js
@@ -2,10 +2,10 @@ const express = require("express");
 const { createTournament, getOneTournament, getAllTournament, updateTournament, deleteTournament } = require("../controllers/TournamentController");
 const router = express.Router();
 
-router.route("/add").post(createTournament);
-router.route("/getTournament/{id}").get(getOneTournament);
+router.route("/addTournament").post(createTournament);
+router.route("/getTournament/:id").get(getOneTournament);
 router.route("/getAllTournament").get(getAllTournament);
 router.route("/updateTournament/").patch(updateTournament);
-router.route("/deleteTournament/{id}").delete(deleteTournament)
+router.route("/deleteTournament/:id").delete(deleteTournament)
 
 module.exports = router;
