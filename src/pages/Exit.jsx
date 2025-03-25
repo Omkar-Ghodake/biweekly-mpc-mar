0a2b94388b32bd5a629a39cd53f6a49ca3565549
rishabhk5236 <rishabhk5236@gmail.com>
2025-03-25 23:44:53 +0530

    added Background in Landing and exit page

--- src/pages/Exit.jsx
+++ src/pages/Exit.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import ExitVideo from '/public/Exit_BG.mp4';
+import '../Styles/Landing_Exit.css';
+
 
 const Exit = () => {
-  return <div>Exit</div>
+  return <div>
+
+    <div className="relative w-full h-screen overflow-hidden bg-blur">
+          {/* Background Video */}
+          <video
+            className="absolute inset-0 w-full h-full object-cover opacity-80"
+            autoPlay
+            muted
+          >
+            <source src={ExitVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          
+          {/* Overlay Content */}
+          <div className="absolute inset-0 flex mt-40 justify-center ">
+            <div className='exit_content text-center'>
+            <h1>Thank you</h1>
+            </div>
+            
+          </div>
+        </div>
+  </div>
 }
 
 export default Exit
