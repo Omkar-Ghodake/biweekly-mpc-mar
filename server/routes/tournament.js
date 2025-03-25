7dc6d28c535397980294b665ff384c839e6ae518
Vishnu Menon <menonvishnu26@gmail.com>
2025-03-25 17:39:22 +0530

    Minor fixes

--- server/routes/tournament.js
+++ server/routes/tournament.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const { createTournament, getOneTournament, getAllTournament, updateTournament, deleteTournament } = require("../controllers/TournamentController.js");
+const {
+  createTournament,
+  getOneTournament,
+  getAllTournament,
+  updateTournament,
+  deleteTournament,
+} = require("../controllers/TournamentController.js");
 const router = express.Router();
-const upload = require("../middleware/multer.middleware.js")
+const upload = require("../middleware/multer.middleware.js");
 
-router.route("/addTournament").post(upload.single("logo"),createTournament); 
+router.route("/addTournament").post(upload.single("logo"), createTournament);
 router.route("/getTournament/:id").get(getOneTournament);
 router.route("/getAllTournament").get(getAllTournament);
-router.route("/updateTournament/").patch(updateTournament);
-router.route("/deleteTournament/:id").delete(deleteTournament)
+router.route("/updateTournament/:id").patch(updateTournament);
+router.route("/deleteTournament/:id").delete(deleteTournament);
 
 module.exports = router;
-
-
-
-
-
