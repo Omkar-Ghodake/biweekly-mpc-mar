2f1b3a27fca608b3a3dc114dbcbb6ae90b0f217c
Omkar-Ghodake <omkar.a.g.78@gmail.com>
2025-03-26 23:16:19 +0530

    created toast notification context

---
+++ src/layouts/ToastNotification.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import ball from '../assets/BallLoader.png'
+import { motion } from 'framer-motion'
+import { ToastContext } from '../context/ToastProvider'
+
+const ToastNotification = () => {
+  const { toastState } = useContext(ToastContext)
+
+  return (
+    <motion.div
+      initial={{ x: '150%' }}
+      animate={{ x: toastState?.display ? 0 : '150%' }}
+      // transition={{ duration: 1, ease: 'easeInOut' }}
+      className='fixed top-5 right-0 bg-white w-fit h-10 shadow-inner shadow-gray-500/80 flex items-center pl-5'
+    >
+      <img
+        src={ball}
+        alt=''
+        className='absolute h-[150%] -translate-x-full animate-spin-reverse'
+      />
+      <span
+        className={`w-full text-right px-2 font-medium text-lg ${
+          toastState.type === 'error' && 'text-red-500'
+        }`}
+      >
+        {toastState?.message}
+      </span>
+    </motion.div>
+  )
+}
+
+export default ToastNotification
