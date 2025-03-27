66cf079055ad7eba24d8f75afd0410ad29d7f35c
Omkar-Ghodake <omkar.a.g.78@gmail.com>
2025-03-27 15:44:55 +0530

    added previous biweekly data file

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import background from '../assets/background.jpg'
+import Button from '../components/Button'
+import { Link } from 'react-router'
 
 const Team = () => {
-  return <div>Team</div>
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <img
+        src={background}
+        alt=''
+        className='fixed inset-0 h-screen w-screen brightness-75'
+      />
+
+      <img
+        src='/Team Photo MPC.png'
+        alt=''
+        className='absolute -bottom-16 z-30 object-cover'
+      />
+
+      <Link to={'/tournaments'} className=' absolute right-10 top-6 z-40 h-fit'>
+        <Button className={''}>Tournaments</Button>
+      </Link>
+    </div>
+  )
 }
 
 export default Team
