b713716f112288a1ff072d920cb35ca3ab1c4e58
Bhavya Momaya <bhavya10.BM@gmail.com>
2025-03-31 12:08:49 +0530

    Cross Icon Final

--- src/layouts/Modal/Modal.jsx
+++ src/layouts/Modal/Modal.jsx
@@ -27,7 +27,7 @@ const Modal = ({ children, className, afterClosing }) => {
               closeModal();
               afterClosing && afterClosing();
             }}
-            className="cursor-pointer absolute -right-10 border border-white  text-white p-1 font-bold rounded-full -top-5 text-2xl hover:text-black/50 hover:bg-red-500 duration-150"
+            className="cursor-pointer absolute -right-10 border border-white  text-white p-1 font-bold rounded-full -top-5 text-2xl hover:text-white/50 hover:bg-blue-900 duration-150"
           />
         </div>
       </motion.div>
