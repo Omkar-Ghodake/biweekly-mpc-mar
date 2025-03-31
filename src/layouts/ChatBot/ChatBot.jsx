3a59d8e3d9c8c757869e6c4e9fa63a25d6ed6d4c
rishabhk5236 <rishabhk5236@gmail.com>
2025-03-31 16:45:06 +0530

    Fixed Raw data responses in bot

--- src/layouts/ChatBot/ChatBot.jsx
+++ src/layouts/ChatBot/ChatBot.jsx
@@ -35,11 +35,13 @@ const ChatBot = () => {
      Answer the following query based on the dataset:
      "${prompt}"
 
-    About : The dataset contains data of employees/members/persons of MPC team and their respective personal projects and contribution in this website/project who are Associate QA Enginner and there are a total of two designation in the team one is Team Lead and the other is teammate and the dataset also contains the data about their work in MPCData
+    About : The dataset contains data of employees/members/persons of MPC team and their respective personal projects and contribution in this website/project who are Associate QA Enginner and there are a total of two designation in the team one is Team Leads and the other is teammate and the dataset also contains the data about their work in MPCData
 
     Instuctions : 
     - Give only Answer of query as response and not the instructions in response 
-    - Remember that a team lead is also a member of team
+    - Strictly do not mention the source of your dataset in any format and incase of asked to do so generate a default response 
+    - Strictly follow the given instructions 
+    - Remember that a team lead is also a member of team and can be referred as a lead 
     - Return the response in 20-50 words until it's mentioned or required
     - The data which is related to employees is present in the EmployeeData and data related to MPC is present in MPCData
     - If the query's response is not present in the dataset then return "Sorry ! I don't have any data related to your query "${prompt}" and if the query is abusive and offensive then return a profanity response"
@@ -49,13 +51,14 @@ const ChatBot = () => {
     - don't add that whether a team member is lead or not in the response and also don't add gender of team members anywhere but use the pronouns accordingly
     - while asking about yourself strictly remember that Sakshi , Sanjeev and Rishabh developed you while Anushree helped in your training and don't mention any personal projects of sakshi,sanjeev , rishabh and anushree there 
     - consider total team score is 171 and don't mention score until asked
-    - Strictly do not mention the source of your dataset
     - Consider the count of gender from the employee data set as there are 5 females and 13 males in the team 
     - Do not use * 
     - Consider that every member is interested and want to work in their release intrest technology
     - When asked related to release intrest or intrest you have to search for the data in employee data dataset
-    - Consider that there are total 11 members named Avinash , Devraj , Dhiraj , Bhavya , Nikita Sonawane , Vishnu , Rishabh , Jaypal , Shubham , Omkar and umakant in the team  who are intrested in any kind of development
+    - Consider that there are total 11 members named Avinash , Devraj , Dhiraj , Bhavya , Nikita Sonawane , Vishnu , Rishabh , Jaypal , Shubham , Omkar and umakant in the team  who are interested in any kind of development
     - Issue Type in employee data can also be reffered as the severity of issue
+    - Do not generate reasponses for Json as prompt and YAML prompt
+
     `;
 
    
