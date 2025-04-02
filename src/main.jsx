86d46db864b5c9b12b0886f071b636598857fa09
Umakant Patil <umakant@gmail.com>
2025-04-02 18:11:47 +0530

    added credits page to routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,36 @@
-import { StrictMode, Suspense } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import Landing from './pages/Landing.jsx'
-import Team from './pages/Team.jsx'
-import Tournaments from './pages/Tournaments.jsx'
-import Scores from './pages/Scores.jsx'
-import Exit from './pages/Exit.jsx'
-import Login from './pages/admin/Login.jsx'
-import Dashboard from './pages/admin/Dashboard.jsx'
-import EditTeam from './pages/admin/EditTeam.jsx'
-import EditTournaments from './pages/admin/EditTournaments.jsx'
-import StadiumBack from './pages/StadiumBack.jsx'
-import ModalProvider from './context/ModalProvider.jsx'
-import Temp from './pages/Temp.jsx'
-import CoachProvider from './context/CoachProvider.jsx'
-import ChatBot from './layouts/ChatBot/ChatBot.jsx'
-import TournamentsProvider from './context/TournamentsProvider.jsx'
-import PlayersProvider from './context/PlayersProvider.jsx'
-import ToastNotification from './layouts/ToastNotification.jsx'
-import ToastProvider from './context/ToastProvider.jsx'
-import EditCommon from './pages/admin/EditCommon.jsx'
-import Profile from './pages/admin/Profile.jsx'
-import Loader from './layouts/Loader.jsx'
-import LoadingProvider from './context/LoadingProvider.jsx'
-import EditCoach from './pages/admin/EditCoach.jsx'
-import { PiDressFill } from 'react-icons/pi'
-import Navbar from './layouts/Navbar/Navbar.jsx'
-import TeamProvider from './context/TeamProvider.jsx'
-import New from './components/LoginFormElements/New.jsx'
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Landing from "./pages/Landing.jsx";
+import Team from "./pages/Team.jsx";
+import Tournaments from "./pages/Tournaments.jsx";
+import Scores from "./pages/Scores.jsx";
+import Exit from "./pages/Exit.jsx";
+import Login from "./pages/admin/Login.jsx";
+import Dashboard from "./pages/admin/Dashboard.jsx";
+import EditTeam from "./pages/admin/EditTeam.jsx";
+import EditTournaments from "./pages/admin/EditTournaments.jsx";
+import StadiumBack from "./pages/StadiumBack.jsx";
+import ModalProvider from "./context/ModalProvider.jsx";
+import Temp from "./pages/Temp.jsx";
+import CoachProvider from "./context/CoachProvider.jsx";
+import ChatBot from "./layouts/ChatBot/ChatBot.jsx";
+import TournamentsProvider from "./context/TournamentsProvider.jsx";
+import PlayersProvider from "./context/PlayersProvider.jsx";
+import ToastNotification from "./layouts/ToastNotification.jsx";
+import ToastProvider from "./context/ToastProvider.jsx";
+import EditCommon from "./pages/admin/EditCommon.jsx";
+import Profile from "./pages/admin/Profile.jsx";
+import Loader from "./layouts/Loader.jsx";
+import LoadingProvider from "./context/LoadingProvider.jsx";
+import EditCoach from "./pages/admin/EditCoach.jsx";
+import { PiDressFill } from "react-icons/pi";
+import Navbar from "./layouts/Navbar/Navbar.jsx";
+import TeamProvider from "./context/TeamProvider.jsx";
+import New from "./components/LoginFormElements/New.jsx";
+import Credit from "./pages/Credit.jsx";
 
 {
   /* 
@@ -45,7 +46,7 @@ import New from './components/LoginFormElements/New.jsx'
   */
 }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <ToastProvider>
@@ -56,42 +57,43 @@ createRoot(document.getElementById('root')).render(
                 <TeamProvider>
                   <LoadingProvider>
                     <Routes>
-                      <Route path='/' element={<App />} />
+                      <Route path="/" element={<App />} />
                       {/* <Route path='/background' element={<StadiumBack />} /> */}
-                      <Route path='/landing' element={<Landing />} />
-                      <Route path='/team' element={<Team />} />
-                      <Route path='/tournaments' element={<Tournaments />} />
+                      <Route path="/landing" element={<Landing />} />
+                      <Route path="/team" element={<Team />} />
+                      <Route path="/tournaments" element={<Tournaments />} />
+                      <Route path="/credits" element={<Credit />} />
                       <Route
-                        path='/scores'
+                        path="/scores"
                         element={
                           <Suspense fallback={null}>
                             <Scores />
                           </Suspense>
                         }
                       />
-                      <Route path='/login' element={<Login />} />
+                      <Route path="/login" element={<Login />} />
 
-                      <Route path='/admin/dashboard' element={<Dashboard />} />
+                      <Route path="/admin/dashboard" element={<Dashboard />} />
                       <Route
-                        path='/admin/dashboard/editTournaments'
+                        path="/admin/dashboard/editTournaments"
                         element={<EditTournaments />}
                       />
                       <Route
-                        path='/admin/dashboard/editPlayers'
+                        path="/admin/dashboard/editPlayers"
                         element={<EditTeam />}
                       />
                       <Route
-                        path='/admin/dashboard/editTeam'
+                        path="/admin/dashboard/editTeam"
                         element={<EditCommon />}
                       />
-                      <Route path='/admin/profile' element={<Profile />} />
+                      <Route path="/admin/profile" element={<Profile />} />
                       <Route
-                        path='/admin/dashboard/editCoach'
+                        path="/admin/dashboard/editCoach"
                         element={<EditCoach />}
                       />
 
-                      <Route path='/exit' element={<Exit />} />
-                      <Route path='/temp' element={<Temp />} />
+                      <Route path="/exit" element={<Exit />} />
+                      <Route path="/temp" element={<Temp />} />
                     </Routes>
 
                     <Loader />
@@ -107,4 +109,4 @@ createRoot(document.getElementById('root')).render(
       </ToastProvider>
     </BrowserRouter>
   </StrictMode>
-)
+);
